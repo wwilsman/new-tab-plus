5774bb203ebd6fe9013d67bcf866c7234af74eff
Wil Wilsman <wil.wilsman@gmail.com>
2017-04-09 15:38:41 -0500

    Add text feild component

---
+++ src/components/TextField/TextField.js
@@ -0,0 +1,59 @@
+import React, { Component, PropTypes } from 'react';
+import './TextField.css';
+
+class TextField extends Component {
+  static propTypes = {
+    label: PropTypes.string.isRequired,
+    placeholder: PropTypes.string,
+    value: PropTypes.string,
+    onChangeText: PropTypes.func,
+    onChange: PropTypes.func,
+    onEnter: PropTypes.func
+  };
+
+  _handleChange = (e) => {
+    const {
+      onChange,
+      onChangeText
+    } = this.props;
+
+    onChange && onChange(e);
+    onChangeText && onChangeText(e.target.value);
+  }
+
+  _handleKeyDown = (e) => {
+    const {
+      onEnter
+    } = this.props;
+
+    if (onEnter && e.which === 13) {
+      onEnter();
+    }
+  }
+
+  render() {
+    const {
+      value,
+      placeholder,
+      label
+    } = this.props;
+
+    return (
+      <div className="TextField">
+        <label className="TextField__label">
+          {label}
+        </label>
+
+        <input
+            className="TextField__input"
+            onChange={this._handleChange}
+            onKeyDown={this._handleKeyDown}
+            placeholder={placeholder}
+            value={value}
+        />
+      </div>
+    );
+  }
+}
+
+export default TextField;
