ca29c875bbe9fa41f8e87119e6122cc23b8ccb01
Wil Wilsman <wil@frontside.io>
2017-05-08 20:46:00 -0500

    Use background for shortcut icons

--- src/components/Shortcut/Shortcut.js
+++ src/components/Shortcut/Shortcut.js
@@ -4,7 +4,13 @@ import './Shortcut.css';
 const Shortcut = ({ name, icon, url }) => (
   <div className="Shortcut">
     <a href={url}>
-      <img className="Shortcut__icon" alt={name} src={icon}/>
+      <div
+          className="Shortcut__icon"
+          style={{
+            backgroundImage: `url('${icon}')`
+          }}
+      />
+
       <div className="Shortcut__name">{name}</div>
     </a>
   </div>
